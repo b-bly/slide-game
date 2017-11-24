fef7fc5484a77f4086efd3d4faa3ae87c98d0125
Brendt Bly <brendt.bly@outlook.com>
2017-11-24 15:58:58 -0600

    empty space added

--- classes/board.js
+++ classes/board.js
@@ -6,19 +6,24 @@ class Board {
         this.draw();
     }
     draw() {
+        const emptySquare = Math.floor(Math.random() * this.totalPieces);
+
+        
         for (let j = 0; j < this.piecesPerSide; j++) {
             for (let i = 0; i < this.piecesPerSide; i++) {
                 //    constructor(width, height, color, x, y, number) {
                 const x = (i * (PIECE_WIDTH + PIECE_MARGIN));
-                const y = (PIECE_WIDTH + PIECE_MARGIN) * j + BOARD_MARGIN_Y; 
+                const y = (PIECE_WIDTH + PIECE_MARGIN) * j + BOARD_MARGIN_Y;
+                const number = (i + 1) + (j * this.piecesPerSide);
+                const empty = emptySquare == number;
                 let piece = new Piece(PIECE_WIDTH, PIECE_WIDTH, PIECE_COLOR,
-                    x, y,
-                    (i + 1) * (j + 1));
+                    x, y, number, empty);
                 this.piecesArray.push(piece);
-                piece.update();
+                if (empty == false) {
+                    piece.update();
+                }
             }
         }
-
     }
 }
 const PIECES_PER_SIDE = 5;
--- classes/piece.js
+++ classes/piece.js
@@ -1,15 +1,24 @@
 class Piece {
-    constructor(width, height, color, x, y, number) {
+    constructor(width, height, color, x, y, number, empty) {
         this.width = width;
         this.height = height;
         this.color = color;
         this.x = x;
         this.y = y;
         this.number = number;
+        this.empty = empty;
     }
     update() {
+        //fill rectangle
         this.ctx = game.context;
         this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        //write font
+        this.ctx.font = '20px Ariel';
+        this.ctx.fillStyle = 'white';
+        this.ctx.textAlign = 'center';
+        const x = this.x + PIECE_WIDTH/2;
+        const y = this.y + PIECE_WIDTH/1.5;
+        this.ctx.fillText(this.number.toString(), x, y);
     }
 }
