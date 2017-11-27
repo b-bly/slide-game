b1e933c65071094e51221a127060ff5c06f31418
Brendt Bly <brendt.bly@outlook.com>
2017-11-27 15:29:47 -0600

    empty space is now random.  reorganized code.

--- classes/board.js
+++ classes/board.js
@@ -11,8 +11,8 @@ class Board {
 
     create() {
         //create random numbers array.
-        const randomArray = [this.emptyNumber];
-        const numberOfSquares = this.piecesPerSide * this.piecesPerSide - 1;
+        const randomArray = [];
+        const numberOfSquares = this.piecesPerSide * this.piecesPerSide;
         for (let i = 0; i < numberOfSquares; i++) {
             let rand = Math.floor(Math.random() * this.totalPieces) + 1;
             while (randomArray.includes(rand)) {
@@ -23,32 +23,17 @@ class Board {
 
         for (let j = 0; j < this.piecesPerSide; j++) {
             for (let i = 0; i < this.piecesPerSide; i++) {
-                let counter = 0;
                 //    constructor(width, height, color, x, y, number) {
                 const x = (i * (PIECE_WIDTH + PIECE_MARGIN));
                 const y = (PIECE_WIDTH + PIECE_MARGIN) * j + BOARD_MARGIN_Y;
                 const number = randomArray.pop();
                 const empty = this.emptyNumber == number;
-               
-    
                 const piece = new Piece(PIECE_WIDTH, PIECE_WIDTH, PIECE_COLOR,
                     x, y, number, empty);
-             
-                this.piecesArray.push(piece);
+                             this.piecesArray.push(piece);
                 if (empty) {
                     this.emptySquare = Object.assign({}, piece);
-                    console.log('the empty created ');
-                    console.log(this.emptySquare);
-                    
-                    
                 }
-                if (j == this.piecesPerSide - 1 && i == this.piecesPerSide - 1) {
-                    console.log('last square: ');
-                    
-                    console.log('last square: ', piece);
-                    
-                }
-                counter++;
             }
         }
     }
@@ -61,8 +46,4 @@ class Board {
         });
     }
 }
-const PIECES_PER_SIDE = 5;
-const PIECE_WIDTH = 50;
-const PIECE_COLOR = 'blue';
-const BOARD_MARGIN_Y = 50;
-const PIECE_MARGIN = 10;
+
---
+++ data.js
@@ -0,0 +1,5 @@
+const PIECES_PER_SIDE = 5;
+const PIECE_WIDTH = 50;
+const PIECE_COLOR = 'blue';
+const BOARD_MARGIN_Y = 50;
+const PIECE_MARGIN = 10;
